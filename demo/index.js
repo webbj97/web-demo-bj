7749c4bb548cb793b67df11d6282d1c9171b2a89
yuguang <yuguang@gaoding.com>
2019-12-09 16:24:35 +0800

    删除无用代码

--- demo/index.js
+++ demo/index.js
@@ -1,32 +1,49 @@
-// promise
-
-// function timeout(ms) {
+// const a = [
+//     {name: 123, title: '我',subs:[1,2]},{name: 123, title: '我',subs:[1,2]},
+//     {name: 1, title: '他',subs:[1,2,3]},{name: 1, title: '你',subs:[1]},
+// ]
+// function unionTitle(a){
 //     return new Promise((resolve, reject) => {
-//         console.log('1')
-//         resolve()
-//         // setTimeout(resolve, ms);
-//         console.log('3')
-//     });
+//         resolve(a);
+//     }).then((val)=>{
+//         var res = new Map()
+//         return val.filter(a => {
+//             return !res.has(a.title) && res.set(a.title, 1);
+//         })
+//     }).then((val)=>{
+//         var min = val[0].subs
+//         val.forEach(e => {
+//             if(e.subs.length < min.length){
+//                 min = e
+//             }
+//         });
+//         return min
+//     })
 // }
 
-// timeout(100).then(() => {
-//     console.log('2');
-// }).catch(()=>{
-//     console.log('error')
-// });
+// function* aaa(){
+//     yield '111'
+//     yield '222'
+//     yield console.log('3')
+//     yield console.log('4')
+//     return console.log('5')
+// }
+// var res = aaa()
+// res.next()
+// res.next()
+// res.next()
+// res.next()
+// res.next()
+// res.next()
 
-// console.log('4')
-
-const p1 = new Promise(function(resolve, reject) {
-    setTimeout(() => {
-        reject('余光测试')
-    }, 3000);
-})
-
-const p2 = new Promise(function(resolve, reject) {
-    setTimeout(() => {
-        resolve(p1)
-    }, 1000);
-})
-
-p2.then(res => console.log(res+'1')).catch((err) => console.log(err))
+function* demo() {
+    // console.log('Hello' + yield); // SyntaxError
+    // console.log('Hello' + yield 123); // SyntaxError
+  
+    console.log('Hello' + (yield)); // OK
+    console.log('Hello' + (yield 123)); // OK
+}
+var demoData = demo()
+demoData.next()
+demoData.next()
+demoData.next()
