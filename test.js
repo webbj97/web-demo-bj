7d150d797a5a20d1173fffd006403fd435614ba0
yuguang <yuguang@gaoding.com>
2019-12-24 14:39:04 +0800

    优化淘宝静态导航

--- test.js
+++ test.js
@@ -1,10 +1,27 @@
-const inp = [
-    ['foo', 'bar'],
-    ['baz', 42]
-];
-var obj = {}
-for(key of inp){
-    obj[key[0]] = key[1]
+function Foo(name) {
+    this.name = name;
+    console.log('Foo:', this.name);
 }
-console.log('obj:', obj);
+Foo.prototype.myName = function() {
+    console.log('Foo-myName:', this.name);
+};
 
+function Bar(name, label) {
+    Foo.call(this, name);
+    this.label = label;
+    console.log('Bar:', this.label)
+}
+Bar.prototype = Object.create(Foo.prototype);
+
+Bar.prototype.myLabel = function() {
+    console.log('Bar-myLabel:',this.label+1)
+    this.label+='1'
+    return this.label;
+};
+var a = new Bar("a", "obj a");
+var b = new Bar("b", "obj b");
+a.myName(); // "a"
+a.myLabel(); // "obj a"
+console.log('---')
+b.myName(); // "a"
+b.myLabel(); // "obj a"
---
+++ 静态官网/淘宝首屏/js/index.js
@@ -0,0 +1,31 @@
+var oNavContentData = [
+    { title: '标题1', list1: [1, 2, 3, 4, 5], list2: [1, 2, 3, 4, 5], list3: [1, 2, 3, 4, 5] },
+    { title: '标题2', list1: [1, 2, 3, 4, 5], list2: [1, 2, 3, 4, 5], list3: [1, 2, 3, 4, 5] },
+    { title: '标题3', list1: [1, 2, 3, 4, 5], list2: [1, 2, 3, 4, 5], list3: [1, 2, 3, 4, 5] },
+    { title: '标题4', list1: [1, 2, 3, 4, 5], list2: [1, 2, 3, 4, 5], list3: [1, 2, 3, 4, 5] },
+    { title: '标题5', list1: [1, 2, 3, 4, 5], list2: [1, 2, 3, 4, 5], list3: [1, 2, 3, 4, 5] },
+    { title: '标题6', list1: [1, 2, 3, 4, 5], list2: [1, 2, 3, 4, 5], list3: [1, 2, 3, 4, 5] },
+    { title: '标题7', list1: [1, 2, 3, 4, 5], list2: [1, 2, 3, 4, 5], list3: [1, 2, 3, 4, 5] },
+    { title: '标题8', list1: [1, 2, 3, 4, 5], list2: [1, 2, 3, 4, 5], list3: [1, 2, 3, 4, 5] },
+    { title: '标题9', list1: [1, 2, 3, 4, 5], list2: [1, 2, 3, 4, 5], list3: [1, 2, 3, 4, 5] },
+    { title: '标题10', list1: [1, 2, 3, 4, 5], list2: [1, 2, 3, 4, 5], list3: [1, 2, 3, 4, 5] },
+    { title: '标题11', list1: [1, 2, 3, 4, 5], list2: [1, 2, 3, 4, 5], list3: [1, 2, 3, 4, 5] },
+    { title: '标题12', list1: [1, 2, 3, 4, 5], list2: [1, 2, 3, 4, 5], list3: [1, 2, 3, 4, 5] },
+    { title: '标题13', list1: [1, 2, 3, 4, 5], list2: [1, 2, 3, 4, 5], list3: [1, 2, 3, 4, 5] },
+    { title: '标题14', list1: [1, 2, 3, 4, 5], list2: [1, 2, 3, 4, 5], list3: [1, 2, 3, 4, 5] },
+]
+var oNavItemBox = document.getElementById('left-items');
+var oNavItemLi = document.querySelectorAll('.left-items-li');//列表内容
+var oNavContent = document.getElementById('left-items-content');//内容区域
+
+oNavItemLi.forEach((item, index) => {
+    item.onmouseover = function (e) {
+        oNavContent.style.display = 'block'
+        oNavContent.innerText = oNavContentData[index].title
+    }
+    
+});
+// oNavItemBox.onmouseout = function (e) {
+//     oNavContent.style.display = 'none'
+//     // oNavContent.innerText = oNavContentData[index].title
+// }
